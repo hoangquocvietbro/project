60620df9065ad20b1b9e96666068b1314351991d
Hoàng Quốc Việt <74923175+hoangquocvietbro@users.noreply.github.com>
2025-01-17 14:23:41 +0700

    Create Route.php

---
+++ Route.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::redirect('.env', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('wp-login.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('wp-admin', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('composer.lock', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('yarn.lock', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('xmlrpc.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('backup/wp-config.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('secrets/api_keys.json', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('docker_run.sh', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('lara/time.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('crm/vendor/phpunit/phpunit/src/Util/PHP/eval-stdin.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('secrets/sparkpost.json', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('config/settings.json', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('apis/lara/info.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('globes_admin', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('phpadmin/phpinfo.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('media/service/phpinfo.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('php.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
+Route::redirect('backend/database.php', 'https://www.youtube.com/watch?v=96p6nP97wWM');
